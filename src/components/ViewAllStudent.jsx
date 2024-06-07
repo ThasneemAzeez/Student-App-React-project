22b14f242b3d98f1956973d38f6608e6d7197b0a
ThasneemAzeez <thasneemazeez2001@gmail.com>
2024-06-07 14:22:15 +0530

    student app

---
+++ src/components/ViewAllStudent.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import NavigationBar from './NavigationBar'
+
+const ViewAllStudent = () => {
+    return (
+        <div>
+            <NavigationBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day.</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                            <div class="card">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnfrlXL1POKB6chHdW-GclSvMIvKMA8FME1w&s" height="250px" class="card-img-top" alt="..."></img>
+                                        <div class="card-body">
+                                            <h5 class="card-title">ABC school</h5>
+                                            <p class="card-text">Inspiring Excellence Every Day</p>
+                                            <a href="#" class="btn btn-primary">Click for enquiry</a>
+                                        </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAllStudent
